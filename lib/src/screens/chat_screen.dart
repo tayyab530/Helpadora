ef6631936cca00a25b382a453bb55db2e01d9919
Tayyab Ahmed <mmtayyab530@gmail.com>
2021-03-18 16:06:50 +0500

    chat_screen setuped

---
+++ lib/src/screens/chat_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  static const routeName = '/chat';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Query Title'),
+      ),
+    );
+  }
+}
