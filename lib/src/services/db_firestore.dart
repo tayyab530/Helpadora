0df8b1544f549444427f2ce5a31c685903f8b8aa
Tayyab Ahmed <mmtayyab530@gmail.com>
2021-04-07 16:23:53 +0500

    firesoter

---
+++ lib/src/services/db_firestore.dart
@@ -0,0 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class DbFirestore {
+  final _firebase = FirebaseFirestore.instance;
+
+  registerData() {}
+}
