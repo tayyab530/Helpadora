0783342eb20395ba8140dd11d43ea347818a857b
Tayyab Ahmed <mmtayyab530@gmail.com>
2021-06-14 12:26:18 +0500

    query notifier setup

--- lib/src/notifiers/queries.dart
+++ lib/src/notifiers/queries.dart
@@ -1,3 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:helpadora/src/widgets/list_of_queries.dart';
 
-class Queries with ChangeNotifier {}
+class Queries with ChangeNotifier {
+  List<QueryDocumentSnapshot> _listOfQueries;
+
+  List<QueryDocumentSnapshot> get listOfQueries => _listOfQueries;
+
+  searchQueries(String _seachContent) {
+    _listOfQueries = _listOfQueries
+        .where(
+          (query) => query.data()['title'].contains(_seachContent),
+        )
+        .toList();
+    notifyListeners();
+  }
+}
